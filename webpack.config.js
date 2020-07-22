32afd29406b7a791116115f269de03448ad16a59
RoxanaDev <roxana.balanm@gmail.com>
2020-07-22 20:16:12 +0200

    Github pages needs the build to be included and present at root folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,40 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const CLIENT_DIR = path.join(__dirname, "./src/client");
-const BUILD_DIR = path.join(__dirname, "./src/build");
+const BUILD_DIR = path.join(__dirname, "./build");
 
 module.exports = {
-    entry: path.join(CLIENT_DIR, "index.tsx"), //Define the entry point for the project
-    output: {   //Define where the compliation package would be placed
-        filename: "bundle.js",
-        path: BUILD_DIR
-    },
-    
-    mode: "production",
+  entry: path.join(CLIENT_DIR, "index.tsx"), //Define the entry point for the project
+  output: {
+    //Define where the compliation package would be placed
+    filename: "bundle.js",
+    path: BUILD_DIR,
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    // http://blog.teamtreehouse.com/introduction-source-maps
-    devtool: "source-map",
+  mode: "production",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  // Enable sourcemaps for debugging webpack's output.
+  // http://blog.teamtreehouse.com/introduction-source-maps
+  devtool: "source-map",
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My first React app',
-            template: path.join(CLIENT_DIR, "index.html")
-          })
-    ]
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My first React app",
+      template: path.join(CLIENT_DIR, "index.html"),
+    }),
+  ],
 };
