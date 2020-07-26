3d86976f4af885dc28ce75a22414ddf995a72278
RoxanaDev <roxana.balanm@gmail.com>
2020-07-26 19:20:47 +0200

    Add initial state management and render of the newly added components.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,13 +2,39 @@ import * as React from "react";
 import Header from "./components/Header";
 import SimpleComponent from "./components/SimpleComponent";
 import styled from "styled-components";
+import SideNavigation from "./components/SideNavigation";
+import Sections from "./constants/Sections";
+import { SectionTypes } from "./types/types";
 
-export default class App extends React.Component {
+interface AppState {
+  currentSection: SectionTypes;
+}
+
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentSection: SectionTypes.ARTICLES,
+    };
+  }
+
+  private onSelectCurrentSection(section: SectionTypes): void {
+    this.setState({
+      currentSection: section,
+    });
+  }
+
   render() {
     return (
       <AppContainer>
         <Header />
-        <SimpleComponent text={"This is another simple component"} />
+        <ContentContainer>
+          <SideNavigation
+            sections={Sections.getSections()}
+            onSelectSection={this.onSelectCurrentSection}
+          />
+          <SimpleComponent text={"This is another simple component"} />
+        </ContentContainer>
       </AppContainer>
     );
   }
@@ -19,4 +45,10 @@ const AppContainer = styled.div`
   height: 100vh;
   min-width: 100%;
   min-height: 100%;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  height: inherit;
+  max-width: fit-content;
 `;
